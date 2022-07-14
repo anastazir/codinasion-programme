e9a6d9772e772bf701d4883e0554920e2befcaf9
Anas Tazir <atazir2018@gmail.com>
2022-07-14 20:14:44 +0530

    add js code to convert lowercase string to uppercase string

---
+++ programme/lowercase-to-uppercase/lowercase-to-uppercase.js
@@ -0,0 +1,10 @@
+// Write a programme to convert lowercase string to uppercase string
+
+const string = "hii there";
+
+function lowercase_to_uppercase(str){
+    return str.toUpperCase();
+}
+
+console.log("Input  :", string);
+console.log("Output :", lowercase_to_uppercase(string));
